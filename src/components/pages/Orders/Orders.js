03f04364a4cf83d3c1a878a401207aa74afba50a
aikokul <arapovaajko@gmail.com>
2023-10-07 15:26:04 +0600

    order

--- src/components/pages/Orders/Orders.js
+++ src/components/pages/Orders/Orders.js
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ordersImg from "../../../img/image 6 (1).png"
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const Orders = () => {
+    const order=useSelector((state)=>state.order)
+    console.log(order);
+        useEffect(()=>{
+            console.log('order updated:', order);
+        },[])
+
+        // const loadImage = async (url) => {
+        //     const response = await fetch(url);
+        //     const blob = await response.blob();
+        //     return URL.createObjectURL(blob);
+        // };
+
+        // useEffect(()=>{
+        //     const LoadImage=async()=>{
+        //         const orderImages = await Promise.all(
+        //             order.map(async(el)=>{
+        //                 const imageUrl=await loadImage(el.image)
+        //                 return{...el, imageUrl}
+        //             })
+        //         )
+        //     }
+        //     loadImage()
+        // },[order])
+        const [orderWithImages, setOrderWithImages] = useState([]);
+
+        useEffect(() => {
+            const loadImage = async (url) => {
+              try {
+                const response = await fetch(url);
+                const blob = await response.blob();
+                return URL.createObjectURL(blob);
+              } catch (error) {
+                console.error('Error loading image:', error);
+                return null;
+              }
+            };
+        
+            const loadImages = async () => {
+              const ordersWithImages = [];
+              for (const el of order) {
+                const imageUrl = await loadImage(el.image);
+                if (imageUrl) {
+                  ordersWithImages.push({ ...el, imageUrl });
+                }
+              }
+        
+              setOrderWithImages(ordersWithImages);
+            };
+        
+            loadImages();
+          }, [order]);
+        
+          useEffect(() => {
+            console.log('orderWithImages updated:', orderWithImages);
+          }, [orderWithImages]);
+        
     return (
         <div id='orders'>
             <div className='container'>
                 <div className='orders'>
                     <h1 className='orders--h1'>TO ORDER</h1>
                     <div className='orders--blocks'>
-                        <div className='orders--blocks__block'>
-                            <img className='orders--img'src={ordersImg} alt=''/>
+                        {
+                            orderWithImages.map((el,idx)=>
+                                (
+                                <div key={idx}className='orders--blocks__block'>
+                            {el.imageUrl && (
+                                    <img className='orders--img' src={el.imageUrl} alt='' />
+                                )}
                             <div className='orders--blocks__block--texts'>
                                 <div className='orders--blocks__block--texts__text'>
-                                    <p className='order--p1 '>cappucino</p>
-                                    <p className='order--p2'>10.5$</p>
+                                    <p className='order--p1 '>{el.foodName}</p>
+                                    <p className='order--p2'>{el.price}$</p>
                                 </div>
                                 <div className='orders--blocks__block--texts__btns'>
                                     <button className='orders--btn1'>delete order</button>
@@ -25,6 +88,11 @@ const Orders = () => {
                                 </div>
                             </div>
                         </div>
+
+                            )
+                                )
+                        }
+                        
                     </div>
                     <div className='last-btn'>
                         <button className='last-btnn'>to order</button>
--- src/components/redux/actionTypes.js
+++ src/components/redux/actionTypes.js
@@ -1,3 +1,4 @@
 export const GET_IMAGE='GET_IMAGE'
 export const GET_FOOD_NAME='GET_FOOD_NAME'
 export const GET_PRICE='GET_PRICE'
+export const GET_ORDER='GET_ORDER'
